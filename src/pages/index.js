dc9f7dec9610a506d789fa50716b4f0ba7ec0adc
Sofiyana-1811 <ipsitajain18@gmail.com>
2023-04-11 23:44:50 +0530

    created basic card for landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import LandingCard from '../components/LandingCard'
 import WhoAreWe from '../components/WhoAreWe'
 import Footer from '../components/Footer'
 const inter = Inter({ subsets: ['latin'] })
@@ -8,6 +9,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
+     <LandingCard></LandingCard>
      <WhoAreWe></WhoAreWe>
      <Footer></Footer>
     </>
