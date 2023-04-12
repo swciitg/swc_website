068050d0b0597d63b5174400d8270de7fdc79618
dhanesh <dhanesh.v@iitg.ac.in>
2023-04-12 23:10:20 +0530

    Added Card TODO: Carousel

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -2,14 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Cursor from '../components/Cursor'
+import CardCarousel from '../components/CardCarousel'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export default function Products() {
   return (
     <>
       {/* <Cursor nums={10} startColor ='yellow' endColor='orange'/> */}
+      <CardCarousel></CardCarousel>
       Products
     </>
   )
