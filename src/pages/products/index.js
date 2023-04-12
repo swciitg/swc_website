cc6e7283b7230ea4c8e47c2670cbcb26567d9640
Ankit Mahto <76254883+ankit-mahto@users.noreply.github.com>
2023-04-12 19:32:04 +0530

    Added cards and fixed floating text (#4)

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -10,8 +10,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Products() {
   return (
     <>
-    {/* this div is for adjusting top position   Must be included -- 3rem for Header and rest for Navbar*/}
-    <div className='bg-black h-[12rem] sm:h-[15rem] md:h-[18rem] w-full'></div> 
+    {/* this dummy div is for adjusting top position Must be included in every index file-- 3rem for Header and 9rem for Navbar*/}
+    <div className='bg-black h-[12rem] w-full'></div> 
       <div className='text-white font-black bg-black mx-auto text-[3rem] sm:text-[4rem] text-center'>Our Products</div>
       {/* Total max width of card container - 68rem or 68*16px */}
       <div className='flex gap-[1rem] overflow-auto justify-start xl:justify-center justify-items-center px-[0.5rem] pt-[1rem] bg-black'>
