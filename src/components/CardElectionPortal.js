9e447fb9983c3e32d1f27c7fcc0eac6490875937
Ankit Mahto <76254883+ankit-mahto@users.noreply.github.com>
2023-04-14 01:14:21 +0530

    Added responsives cards and fixed floating text (#9)
    * Added cards and fixed floating text
    
    * Cards made responsive

--- src/components/CardElectionPortal.js
+++ src/components/CardElectionPortal.js
@@ -3,19 +3,19 @@ export default function CardElectionPortal() {
   return (
     <div>  
       {/* card Component width-*/}
-      <div className='relative w-[39rem] h-[32rem] rounded-3xl flex-inital'>
+      <div className='relative w-[21rem] h-[25rem] sm:w-[39rem] sm:h-[32rem] rounded-3xl flex-inital'>
         {/* Image part of Card */}
-        <div className='h-4/5 bg-gradient-to-bl from-[#A06DB2] via-[#EEDDA1] to-[#9A5D89]  rounded-t-3xl'></div>
+        <div className='h-3/4 sm:h-4/5 bg-gradient-to-bl from-[#A06DB2] via-[#EEDDA1] to-[#9A5D89]  rounded-t-3xl'></div>
         {/* Footer part of Card */}
-        <div className=' bg-[#321F24] overflow-hidden w-full h-1/5 rounded-b-3xl px-[1rem]'>  
+        <div className=' bg-[#321F24] overflow-auto w-full  h-1/4 sm:h-1/5 rounded-b-3xl px-[1rem] pb-[0.5rem]'>  
           <div>
-            <h1 className='p-[1rem] text-white font-bold'>Election Portal</h1>{/* Title of Card*/}
-            <ul className='text-white/50 flex flex-row flex-start flex-initial flex-wrap gap-[0.5rem] px-[1rem]'>
+            <h1 className='p-[0.5rem] sm:p-[1rem] text-white font-bold'>Election Portal</h1>{/* Title of Card*/}
+            <ul className='text-white/50 flex flex-row flex-start flex-initial flex-wrap gap-[0.5rem] px-[0.5rem] sm:px-[1rem]'>
                 {/* Buttons of Card*/}
-            <li className='rounded-[2.5rem] border-white/50 border px-[0.5rem] bg-[#2B1B1B]'><a href="" target='_blank'>Chrome Web Store</a> </li>
-            <li className='rounded-[2.5rem] border-white/50 border px-[0.5rem] bg-[#2B1B1B]'><a href="" target='_blank'>Product Hunt</a></li>
-            <li className='rounded-[2.5rem] border-white/50 border px-[0.5rem] bg-[#2B1B1B]'><a href="" target='_blank'>Website</a></li>
-            <li className='rounded-[2.5rem] border-white/50 border px-[0.5rem] bg-[#2B1B1B]'><a href="" target='_blank'>Github</a></li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-[0.5rem] bg-[#2B1B1B]'><a href="" target='_blank'>Chrome Web Store</a> </li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-[0.5rem] bg-[#2B1B1B]'><a href="" target='_blank'>Product Hunt</a></li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-[0.5rem] bg-[#2B1B1B]'><a href="" target='_blank'>Website</a></li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-[0.5rem] bg-[#2B1B1B]'><a href="" target='_blank'>Github</a></li>
             </ul>
           </div>
         </div>
--- src/components/CardSwcJourney.js
+++ src/components/CardSwcJourney.js
@@ -3,19 +3,19 @@ export default function CardSwcJourney() {
   return (
     <div>  
       {/* card Component*/}
-      <div className='relative w-[28rem] h-[32rem] rounded-3xl flex-initial'>
+      <div className='relative w-[21rem] h-[25rem] sm:w-[28rem] sm:h-[32rem] rounded-3xl flex-initial'>
         {/* Image part of Card */}
         <div className='h-3/4 bg-gradient-to-bl from-[#CBF4B7] via-[#93DAA5] to-[#36A27C]  rounded-t-3xl'></div>
         {/* Footer part of Card */}
-        <div className=' bg-[#1F322B] overflow-hidden w-full h-1/4 rounded-b-3xl px-[1rem]'>  
+        <div className=' bg-[#1F322B] overflow-auto w-full h-1/4 rounded-b-3xl px-[1rem] pb-[0.5rem]'>  
           <div>
-            <h1 className='p-[1rem] text-white font-bold'>SWC Journeys</h1> {/* Title of Card*/}
-            <ul className='text-white/50 flex flex-row flex-start flex-initial flex-wrap gap-[0.5rem] px-[1rem]'>
+            <div className='p-[0.5rem] sm:p-[1rem] text-white font-bold sticky'>SWC Journeys</div> {/* Title of Card*/}
+            <ul className='text-white/50 flex flex-row flex-start flex-initial flex-wrap gap-[0.5rem] px-[0.5rem] sm:px-[1rem]'>
               {/* Buttons of Card*/}
-            <li className='rounded-[2.5rem] border-white/50 border px-[0.5rem] bg-[#1B2B25]'><a href="" target='_blank'>Chrome Web Store</a> </li>
-            <li className='rounded-[2.5rem] border-white/50 border px-[0.5rem] bg-[#1B2B25]'><a href="" target='_blank'>Product Hunt</a></li>
-            <li className='rounded-[2.5rem] border-white/50 border px-[0.5rem] bg-[#1B2B25]'><a href="" target='_blank'>Website</a></li>
-            <li className='rounded-[2.5rem] border-white/50 border px-[0.5rem] bg-[#1B2B25]'><a href="" target='_blank'>Github</a></li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-[0.5rem] bg-[#1B2B25]'><a href="" target='_blank'>Chrome Web Store</a> </li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-[0.5rem] bg-[#1B2B25]'><a href="" target='_blank'>Product Hunt</a></li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-[0.5rem] bg-[#1B2B25]'><a href="" target='_blank'>Website</a></li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-[0.5rem] bg-[#1B2B25]'><a href="" target='_blank'>Github</a></li>
             </ul>
           </div>
         </div>
