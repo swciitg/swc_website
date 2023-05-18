e30e3a36474d44f73cc767388b43a652770403fd
Sofiyana-1811 <ipsitajain18@gmail.com>
2023-05-18 17:31:34 +0530

    added more product cards

---
+++ src/components/CardPlacementPortal.js
@@ -0,0 +1,24 @@
+import React from 'react'
+export default function CardPlacementPortal() {
+  return (
+    <div>  
+      {/* card Component*/}
+      <div className='flex flex-col relative w-[50rem] h-[23rem] sm:w-[68rem] sm:h-[22rem] rounded-3xl flex-initial'>
+        {/* Image part of Card */}
+        <div className='h-[60%] sm:h-[65%] bg-[url(/cards/placementPortal.jpg)] bg-cover rounded-t-3xl'></div>
+        {/* Footer part of Card */}
+        <div className='bg-[#571A18] overflow-hidden w-full h-fit rounded-b-3xl px-[1rem] pb-[0.5rem]'>  
+          <div>
+            <h1 className='p-[0.5rem] sm:p-[1rem] pb-[0.5rem] pt-[1rem] text-white text-xl font-bold'>Placement Portal</h1>{/* Title of Card*/}
+            <ul className='text-white/50 flex flex-row flex-start flex-initial flex-wrap gap-[0.5rem] pb-[0.5rem] sm:pb-[1rem] px-[0.5rem] sm:px-[1rem]'>
+                {/* Buttons of Card*/}
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-4 py-2.5 bg-black bg-opacity-50'><a href="" target='_blank'>Play Store</a> </li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-4 py-2.5 bg-black bg-opacity-50'><a href="" target='_blank'>Dashboard</a></li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-4 py-2.5 bg-black bg-opacity-50'><a href="" target='_blank'>Github</a></li>
+            </ul>
+          </div>
+        </div>
+      </div> 
+    </div>
+  )
+}
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -5,6 +5,8 @@ import CardSwcJourney from '../../components/CardSwcJourney'
 import CardElectionPortal from '../../components/CardElectionPortal'
 import CardOneStop from '../../components/CardOneStop'
 import CardPlacementStats from '@/components/CardPlacementStats'
+import CardHmcElections from '@/components/CardHmcElections'
+import CardPlacementPortal from '@/components/CardPlacementPortal'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Products({ExperienceData}) {
@@ -12,20 +14,23 @@ export default function Products({ExperienceData}) {
     <>
     {/* this dummy div is for adjusting top position Must be included in every index file-- 3rem for Header and 9rem for Navbar*/}
     <div className='bg-black h-[12rem] w-full'></div> 
-    <div className="flex flex-col h-fit">
+    <div className="flex flex-col h-fit w-[90%] overflow-hidden sm:w-full sm:items-center mx-auto">
       <div className='text-white font-black bg-black mx-auto text-[3rem] sm:text-[4rem] text-center mb-12'>Our Products</div>
       {/* Total max width of card container - 68rem or 68*16px */}
-      <div className='flex gap-4 overflow-auto justify-start xl:justify-center justify-items-center bg-black'>
+      <div className='flex gap-4 overflow-x-auto justify-start xl:justify-center justify-items-center bg-black'>
         <CardSwcJourney/>
         <CardElectionPortal/>
       </div>
-      <div className='flex flex-row justify-center overflow-auto bg-black mb-4'> 
+      <div className='flex flex-row justify-start items-center overflow-auto bg-black sm:mb-4 sm:mt-2'> 
         <CardOneStop/> 
       </div>
       <div className='flex gap-4 overflow-y-hidden justify-start xl:justify-center justify-items-center bg-black h-fit'>
         <CardPlacementStats/>
-        <CardSwcJourney/>
-      </div>    
+        <CardHmcElections/>
+      </div>
+      <div className='flex flex-row justify-start overflow-auto bg-black mb-4 mt-2'> 
+        <CardPlacementPortal/> 
+      </div>
     </div>
       
     </>
