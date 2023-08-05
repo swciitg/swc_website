ca7c923bd270013797a0782367665a4d0ea5d688
Hareesh-Nandigrama <hareeshnandigrama@gmail.com>
2023-08-05 23:11:54 +0530

    hiring page complete

--- src/components/WeAreHiring.js
+++ src/components/WeAreHiring.js
@@ -11,12 +11,19 @@ const job5=cardData.find(job5=>job5.id==='5');
     return (
         <div className="flex flex-col">
             <div className="flex flex-col md:flex-row justify-center items-center mt-8">
+                <div>
                 <div className="flex flex-row justify-center mr-10 sm:flex-col w-80 lg:w-[28rem] font-[Inter] text-5xl  md:text-7xl font-extrabold lg:pl-6 lg:mr-8">
                     <div className="bg-contain  text-transparent bg-clip-text bg-gradient-to-r from-[#CCE3ED] via-[#4D75B5] to-[#AB1C4E] pb-8 pl-4 text-center lg:text-start">
                         <div>We</div>
                          Are Hiring!
                          </div>
+                    
+                </div>  
+                <div className='text-xl text-cyan-500 ml-12 font-[Inter] hover:text-cyan-400'>
+                    <a href='https://forms.office.com/r/X4mxHugR6X'> Link to Apply &#8594; </a>
+                </div>
                 </div>   
+
                 <HiringCards id={job1.id} title={job1.title} description={job1.description} eligibility={job1.eligibility} color={job1.color}></HiringCards>
             </div>
             <div className="flex  flex-col md:flex-row justify-center items-center mt-3 sm:mt-8">
