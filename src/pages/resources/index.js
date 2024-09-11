1c43de656bd1c1c7d7365f41f485ea41bd97a803
Avinash Gupta <guptaavinash302@gmail.com>
2024-09-11 16:59:18 +0530

    team update and resources of hackstack update

--- src/pages/resources/index.js
+++ src/pages/resources/index.js
@@ -15,35 +15,42 @@ const Resources_Data = [
     img: "/swc/flutter.png",
     description:
       " An open-source UI toolkit by Google for creating natively compiled applications across mobile, web, and desktop from a single codebase.",
-      link:"https://www.notion.so/swciitg/Flutter-Workshop-2023-58e463347af34fb9be6f1d5dc06fb26e?pvs=4",
+      link:"https://swciitg.notion.site/Flutter-Hackstack-2024-6274025e81d64ac591a38e1ccc034b81?pvs=25",
     },
   {
     name: "HTML, CSS, JavaScript",
     img: "/swc/html.png",
     description:
       "The foundational trinity of web development: HTML for structure, CSS for styling, and JavaScript for interactivity.",
-      link:"https://swciitg.notion.site/Hackstack-Html-css-js-2023-27109c6b9f2b48a4ac5ecc7bde687570",
+      link:"https://swciitg.notion.site/HackStack-24-HTML-CSS-JavaScript-036b384a13cf4e90a3c786071c3abf31?pvs=25",
     },
   {
     name: "ReactJs",
     img: "/swc/react.png",
     description:
       " A JavaScript library by Facebook for building dynamic user interfaces, emphasizing component-based architecture and efficient rendering.",
-      link:"https://swciitg.notion.site/React-js-Workshop-2023-HackStack-9e75bc6fa7664e45930074ad11cab317",
+      link:"https://swciitg.notion.site/React-Hackstack-2024-b4fee56832de4347aad9c6f182a642cb?pvs=25",
     },
   {
     name: "NodeJs",
     img: "/swc/node.png",
     description:
       " A server-side runtime for executing JavaScript, renowned for its non-blocking, event-driven architecture and scalability.",
-      link:"https://swciitg.notion.site/Node-js-Workshop-2023-HackStack-268dcbe21f8b45fc9016320ddd10aa2f",
+      link:"https://swciitg.notion.site/Node-js-Workshop-2024-299346a043124e8f861afe067cdec323?pvs=25",
+    },
+  {
+      name: "Django",
+      img: "/swc/django.png",
+      description:
+        " A high-level Python web framework, renowned for its rapid development, built-in features, and scalability.",
+        link:"https://swciitg.notion.site/Django-Hackstack-2024-8f4115ccfbf44bcd9b7898c7c0c75550?pvs=25",
     },
   {
     name: "UI/UX",
     img: "/swc/figma.png",
     description:
       " UI (User Interface) focuses on design and the aesthetics of a product, while UX (User Experience) emphasizes its functionality and the overall experience of the user.",
-      link:"https://www.notion.so/swciitg/54316871381e4f3abafaacf967e22d41?v=604ac080329c4adfab7a5ea4bb03890d&pvs=4",
+      link:"https://swciitg.notion.site/UI-UX-Workshop-a4acd98e599946fa8c5ee1f478dba276?pvs=25",
     },
 ]
 
