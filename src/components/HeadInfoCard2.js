e1e1dab38852842cc5512e3e20e5247af9b8f785
Manan <mananvala15@gmail.com>
2026-08-16 05:07:51 +0530

    team page ui fix

--- src/components/HeadInfoCard2.js
+++ src/components/HeadInfoCard2.js
@@ -6,7 +6,7 @@ const HeadInfoCard = ({ pfp, por, name, degree, phno }) => {
     return (
         <div className="w-full bg-black flex flex-row my-4">
             <div className="w-1/3  h-40 md:h-72 md:w-1/5 flex justify-center items-center rounded-xl md:rounded-2xl overflow-hidden bg-gradient-to-b from-teambg2">
-                <Image src={pfp} height={400} width={400} className='object-contain w-full h-full object-center"' unoptimized />
+                <Image src={pfp} height={400} width={400} className="object-contain w-full h-full object-center rounded-xl md:rounded-2xl" unoptimized />
             </div>
             <div className="flex flex-col ml-[5%] md:ml-[10%] h-inherit justify-between">
                 <span className="flex font-['Inter'] font-bold text-xl md:text-4xl whitespace-nowrap">
