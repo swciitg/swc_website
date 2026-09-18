f30b1d693003fa32d2899059a9e14654d7d2ccb2
Manan <mananvala15@gmail.com>
2026-09-18 09:33:50 +0530

    Widen products page cards and fix links
    Widen Senate/Resume/Welfare/Sports board cards to fill their row width,
    center card background images, update website links to swc.iitg.ac.in,
    and remove Github buttons.

--- src/components/CardWelfareBoard.js
+++ src/components/CardWelfareBoard.js
@@ -3,17 +3,16 @@ export default function CardWelfareBoard() {
   return (
     <div>
       {/* card Component*/}
-      <div className='flex flex-col relative w-[21rem] h-[25rem] sm:w-[28rem] sm:h-[32rem] rounded-3xl flex-initial m-0'>
+      <div className='flex flex-col relative w-[21rem] h-[25rem] sm:w-[33.5rem] sm:h-[32rem] rounded-3xl flex-initial m-0'>
         {/* Image part of Card */}
-        <div className='h-[60%] sm:h-[65%] bg-[url(/swc/cards/welfareBoard.png)] bg-cover rounded-t-3xl'></div>
+        <div className='h-[60%] sm:h-[65%] bg-[url(/swc/cards/welfareBoard.png)] bg-cover bg-center rounded-t-3xl'></div>
         {/* Footer part of Card */}
         <div className=' bg-[#2e2f2f] overflow-hidden w-full h-fit rounded-b-3xl px-[1rem]'>
           <div>
             <div className='p-[0.5rem] sm:p-[1rem] text-white text-xl font-bold sticky'>Welfare Board</div> {/* Title of Card*/}
             <ul className='text-white/50 flex flex-row flex-start flex-initial flex-wrap gap-[0.5rem] pb-[0.5rem] sm:pb-[1rem] px-[0.5rem] sm:px-[1rem]'>
               {/* Buttons of Card*/}
-            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-4 py-2.5 bg-black bg-opacity-50 hover:opacity-50 hover:cursor-pointer'><a href="https://welfare.iitg.ac.in" target='_blank'>Website</a> </li>
-            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-4 py-2.5 bg-black bg-opacity-50 hover:opacity-50 hover:cursor-pointer'><a href="https://github.com/swciitg/welfare-board" target='_blank'>Github</a></li>
+            <li className='rounded-[2.5rem] text-sm sm:text-[1rem] border-white/50 border px-4 py-2.5 bg-black bg-opacity-50 hover:opacity-50 hover:cursor-pointer'><a href="https://swc.iitg.ac.in/welfare-board/" target='_blank'>Website</a> </li>
             </ul>
           </div>
         </div>
